6ea66916171f71f36b1e5076bef9d59119df9ac5
Aleman778 <aleman778.maintheria@gmail.com>
2015-09-02 16:44:47 +0200

    Modified GameScene to use javafx.scene.Scene as a variable

--- src/ga/engine/GameScene.java
+++ src/ga/engine/GameScene.java
@@ -1,12 +1,31 @@
 package ga.engine;
 
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 
-public class GameScene extends Scene {
+public class GameScene {
 
-    public GameScene(Parent root) {
-        super(root);
+    private Scene scene;
+    private AnchorPane node;
+    
+    /**
+     * Constructs a Scene from filepath scene
+     * @param filepath path to xml scene
+     */
+    public GameScene(String filepath) {
+        node = new AnchorPane();
+        scene = new Scene(node);
     }
     
+    public javafx.scene.Scene get() {
+        return scene;
+    }
+    
+    public void update() {
+        
+    }
+    
+    public void render() {
+        
+    }
 }
