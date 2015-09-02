d915dec2898043bc53e56ed78f6af7eb9fdec07b
Johan Hedlin <busan.katt@gmail.com>
2015-09-02 15:00:36 +0200

    Add placeholder image and KeyboardHandler to Application

--- src/ga/engine/Application.java
+++ src/ga/engine/Application.java
@@ -1,14 +1,19 @@
 package ga.engine;
 
 import javafx.animation.AnimationTimer;
-import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 
 public class Application extends javafx.application.Application {
     
     private static AnimationTimer gameloop;
     private static Stage window;
-    private static Scene scene;
+    private static GameScene scene;
+    private static final Group root = new Group();
+    private static final KeyboardHandler keyboard = new KeyboardHandler();
     
     @Override
     public void start(Stage primaryStage) {
@@ -18,12 +23,18 @@ public class Application extends javafx.application.Application {
     
     public static void init(Stage window) {
         Application.window = window;
-        Application.scene = null;
+        Application.scene = new GameScene(root);
+        window.setScene(scene);
+        final ImageView background = new ImageView(new Image(Application.class.getResourceAsStream("Placeholder Grass.png")));
+        root.getChildren().add(background);
+        scene.setOnKeyPressed(keyboard);
+        scene.setOnKeyReleased(keyboard);
         gameloop = new AnimationTimer() {
 
             @Override
             public void handle(long now) {
                 //INSERT GAME LOOP HERE! UPDATE / RENDER SCENE!
+                background.setVisible(!keyboard.getPressed(KeyCode.SPACE));
             }
         };
     }
@@ -31,6 +42,8 @@ public class Application extends javafx.application.Application {
     public static void start() {
         gameloop.start();
         window.setTitle("Ga Engine");
+        window.setHeight(250);
+        window.setWidth(250);
         window.show();
     }
     
