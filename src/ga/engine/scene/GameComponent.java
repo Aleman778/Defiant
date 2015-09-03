3af9c2c658b2fc4f7de2b1b826930749d4e02c5e
Aleman778 <aleman778.maintheria@gmail.com>
2015-09-03 17:39:40 +0200

    Incomplete implementation for the GameComponent class

---
+++ src/ga/engine/scene/GameComponent.java
@@ -0,0 +1,17 @@
+package ga.engine.scene;
+
+public class GameComponent {
+    
+    public float x, y;
+    
+    public GameComponent(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public void start() {}
+    
+    public void update() {}
+    
+    public void render() {}
+}
