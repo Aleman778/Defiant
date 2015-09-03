3d15d305c5f2426a5e9472014bcf493966ead0ba
Aleman778 <aleman778.maintheria@gmail.com>
2015-09-03 17:40:05 +0200

    Base functionality for the GameObject class

---
+++ src/ga/engine/scene/GameObject.java
@@ -0,0 +1,25 @@
+package ga.engine.scene;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameObject {
+
+    private final List<GameObject> children;
+    private final List<GameComponent> components;
+    
+    public GameObject() {
+        children = new ArrayList<>();
+        components = new ArrayList<>();
+    }
+    
+    public GameObject addChild(GameObject object) {
+        children.add(object);
+        return object;
+    }
+    
+    public GameObject addComponent(GameComponent component) {
+        components.add(component);
+        return this;
+    }
+}
