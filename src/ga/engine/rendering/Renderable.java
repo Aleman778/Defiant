5bfbe5104e7434abb1943fa3788b3e65a9865cec
Aleman778 <aleman778.maintheria@gmail.com>
2015-09-04 21:55:08 +0200

    Added render method parameter Camera

--- src/ga/engine/rendering/Renderable.java
+++ src/ga/engine/rendering/Renderable.java
@@ -1,10 +1,11 @@
 package ga.engine.rendering;
 
+import ga.engine.scene.Camera;
 import javafx.scene.Node;
 
 public interface Renderable {
     
-    public void render();
+    public void render(Camera camera);
     
     public Node getNode();
 }
