34cf14f300b4aeda9b9904d4d3674bca5a79791a
Aleman778 <aleman778.maintheria@gmail.com>
2015-09-04 21:59:15 +0200

    Fixed render() and update() method

--- src/ga/engine/scene/GameObject.java
+++ src/ga/engine/scene/GameObject.java
@@ -1,14 +1,21 @@
 package ga.engine.scene;
 
+import ga.engine.rendering.Renderable;
 import java.util.ArrayList;
 import java.util.List;
+import javafx.scene.Node;
 
 public class GameObject {
 
+    public float x, y;
+    
     private final List<GameObject> children;
     private final List<GameComponent> components;
+    private Renderable renderable;
     
-    public GameObject() {
+    public GameObject(float x, float y) {
+        this.x = x;
+        this.y = y;
         children = new ArrayList<>();
         components = new ArrayList<>();
     }
@@ -27,11 +34,22 @@ public class GameObject {
         for (GameComponent component: components) {
             component.update();
         }
+        
+        for (GameObject child: children) {
+            child.update();
+        }
     }
     
-    public void render() {
-        for (GameComponent component: components) {
-            component.render();
+    public void render(Camera camera) {
+        if (renderable != null) {
+            renderable.render(camera);
+            Node node = renderable.getNode();
+            node.setTranslateX(x);
+            node.setTranslateY(y);
+        }
+            
+        for (GameObject child: children) {
+            child.render(camera);
         }
     }
 }
