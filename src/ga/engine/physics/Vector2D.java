63ff1b8823026b6b0ec058a06b65bd4b142c9c5a
Johan Hedlin <busan.katt@gmail.com>
2015-09-05 17:10:37 +0200

    Add Vector2D

---
+++ src/ga/engine/physics/Vector2D.java
@@ -0,0 +1,34 @@
+package ga.engine.physics;
+
+public class Vector2D {
+    
+    private double dX = 0, dY = 0;
+
+    public Vector2D(double dX, double dY) {
+        this.dX = dX;
+        this.dY = dY;
+    }
+    
+    public Vector2D() {}
+    
+    public double length() {
+        return Math.sqrt(dX * dX + dY * dY);
+    }
+    
+    public Vector2D add(Vector2D vector) {
+        return new Vector2D(dX + vector.dX, dY + vector.dY);
+    }
+    
+    public Vector2D sub(Vector2D vector) {
+        return new Vector2D(dX - vector.dX, dY - vector.dY);
+    }
+    
+    public Vector2D scale(double scale) {
+        return new Vector2D(dX * scale, dY * scale);
+    }
+    
+    public double dot(Vector2D vector) {
+        return dX * vector.dX + dY * vector.dY;
+    }
+    
+}
