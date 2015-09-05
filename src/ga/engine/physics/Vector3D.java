0dae4c6308d4e149e450d918cd8241384392e889
Aleman778 <aleman778.maintheria@gmail.com>
2015-09-05 17:57:50 +0200

    Added Vector3D class

---
+++ src/ga/engine/physics/Vector3D.java
@@ -0,0 +1,30 @@
+package ga.engine.physics;
+
+public class Vector3D {
+    
+    private double dX = 0, dY = 0, dZ = 0;
+
+    public Vector3D(double dX, double dY, double dZ) {
+        this.dX = dX;
+        this.dY = dY;
+        this.dZ = dZ;
+    }
+    
+    public Vector3D() {}
+    
+    public double length() {
+        return Math.sqrt(dX * dX + dY * dY + dZ * dZ);
+    }
+    
+    public Vector3D add(Vector3D vector) {
+        return new Vector3D(dX + vector.dX, dY + vector.dY, dZ + vector.dZ);
+    }
+    
+    public Vector3D sub(Vector3D vector) {
+        return new Vector3D(dX - vector.dX, dY - vector.dY, dZ - vector.dZ);
+    }
+    
+    public Vector3D scale(double scale) {
+        return new Vector3D(dX * scale, dY * scale, dZ * scale);
+    }
+}
