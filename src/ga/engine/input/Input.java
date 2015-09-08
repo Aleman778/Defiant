38ffbe2242be6d4fd87d1669a24139e4d527093a
Aleman778 <aleman778.maintheria@gmail.com>
2015-09-08 20:35:49 +0200

    Setting up Input class, Keyboard and Mouse

--- src/ga/engine/input/Input.java
+++ src/ga/engine/input/Input.java
@@ -1,8 +1,31 @@
 package ga.engine.input;
 
+import ga.engine.physics.Vector2D;
+import ga.engine.scene.GameScene;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+
 public class Input {
     
-    public Input() {
-        
+    public static MouseButton mouseButton;
+    public static Vector2D mousePosition;
+    public static float lerp = 0.2f;
+    
+    private static Set<KeyCode> keys;
+    private static Set<KeyCode> keysPressed;
+    private static Set<KeyCode> keysReleased;
+    private static Map<String, InputAxis> axis;
+    
+    private final GameScene scene;
+    
+    public Input(GameScene scene) {
+        this.scene = scene;
+        keysPressed = new HashSet<>();
+        keysReleased = new HashSet<>();
+        mousePosition = new Vector2D();
+        mouseButton = MouseButton.NONE;
     }
 }
