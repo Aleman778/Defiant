111595a913e78b9b11d18cb46cb1ba94daa9a742
Aleman778 <aleman778.maintheria@gmail.com>
2015-09-08 20:12:32 +0200

    Added onCollision parameters

--- src/ga/engine/scene/GameComponent.java
+++ src/ga/engine/scene/GameComponent.java
@@ -1,5 +1,8 @@
 package ga.engine.scene;
 
+import ga.engine.physics.Body;
+import ga.engine.physics.Vector2D;
+
 public abstract class GameComponent {
     
     public Transform transform;
@@ -15,6 +18,6 @@ public abstract class GameComponent {
     public void update() {}
     public void lateUpdate() {}
     public void onCollisionEnter() {}
-    public void onCollision() {}
+    public void onCollision(Body body, Vector2D normal, double penetration) {}
     public void onCollisionExit() {}
 }
