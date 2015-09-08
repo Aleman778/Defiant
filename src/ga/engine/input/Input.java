01f4dd1ba0a070649bb79068dbb79f2ab2f534e1
Aleman778 <aleman778.maintheria@gmail.com>
2015-09-08 20:45:08 +0200

    Added methods for updating Axis and pressed keys

--- src/ga/engine/input/Input.java
+++ src/ga/engine/input/Input.java
@@ -2,10 +2,14 @@ package ga.engine.input;
 
 import ga.engine.physics.Vector2D;
 import ga.engine.scene.GameScene;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+import javafx.event.EventHandler;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
 
 public class Input {
@@ -23,9 +27,84 @@ public class Input {
     
     public Input(GameScene scene) {
         this.scene = scene;
+        scene.get().setOnKeyPressed((KeyEvent event) -> {
+            pressedKey(event.getCode());
+        });
+        scene.get().setOnKeyReleased((KeyEvent event) -> {
+            releasedKey(event.getCode());
+        });
+        axis = new HashMap<>();
+        keys = new HashSet<>();
         keysPressed = new HashSet<>();
         keysReleased = new HashSet<>();
         mousePosition = new Vector2D();
         mouseButton = MouseButton.NONE;
+        axis.put("horizontal", new InputAxis(KeyCode.A, KeyCode.D));
+        axis.put("vertical", new InputAxis(KeyCode.W, KeyCode.S));
+    }
+    
+    public void update() {
+        axis.forEach((String t, InputAxis u) -> {
+            Iterator<KeyCode> it = keys.iterator();
+            float val = 0.0f;
+            while (it.hasNext()) {
+                KeyCode key = it.next();
+                if (key.equals(u.getKeyPos()))
+                    val += lerp;
+                if (key.equals(u.getKeyNeg()))
+                    val -= lerp;
+                Math.max(Math.min(lerp, val), -lerp);
+            }
+            
+            if (val == 0.0f) {
+                val -= (float) (Math.signum((int) (u.value * 10.0f)) * lerp) / 10.0f;
+            }
+            
+            u.value += val;
+            u.value = (float) ((int) (u.value * 10.0f)) / 10.0f;
+            u.value = Math.max(Math.min(1.0f, u.value), -1.0f);
+        });
+    }
+    
+    private void pressedKey(KeyCode keyCode) {
+        if (!keys.contains(keyCode))
+            keysPressed.add(keyCode);
+        keys.add(keyCode);
+    }
+    
+    private void releasedKey(KeyCode keyCode) {
+        keys.remove(keyCode);
+        keysReleased.add(keyCode);
+    }
+    
+    public void clearAll() {
+        keys.clear();
+        keysPressed.clear();
+        keysReleased.clear();
+    }
+    
+    public void clear() {
+        keysPressed.clear();
+        keysReleased.clear();
+    }
+    
+    public static boolean getKeyPressed(KeyCode key) {
+        return keysPressed.contains(key);
+    }
+    
+    public static boolean getKeyReleased(KeyCode key) {
+        return keysReleased.contains(key);
+    }
+    
+    public static boolean getKey(KeyCode key) {
+        return keysPressed.contains(key);
+    }
+    
+    public static boolean getMouseButton(MouseButton mb) {
+        return mouseButton.equals(mb);
+    }
+    
+    public static MouseButton getMouseButton() {
+        return mouseButton;
     }
 }
