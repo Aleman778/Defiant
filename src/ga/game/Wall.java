aa319b7c01734f0c8d53b5dc3069e4a1cc4cc71a
Johan Hedlin <busan.katt@gmail.com>
2015-10-03 16:21:18 +0200

    Added Wall

---
+++ src/ga/game/Wall.java
@@ -0,0 +1,16 @@
+package ga.game;
+
+import ga.engine.physics.RigidBody;
+import ga.engine.rendering.TileRenderer;
+import ga.engine.scene.GameObject;
+
+public class Wall {
+
+    public Wall(GameObject root, double x, double y, int width, int height) {
+        GameObject box = new GameObject(x + (width * 32) / 2 - 16, y + (height * 32) / 2 - 16, 0)
+                .addComponent(new TileRenderer("ga/game/Jordlabb.png", width, height));
+        RigidBody body2 = new RigidBody(0);
+        box.addComponent(body2);
+        root.addChild(box);
+    }
+}
