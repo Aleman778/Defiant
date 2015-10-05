aa7139aa70f071e07f7490416bafea6f67f1cdc3
Johan Hedlin <busan.katt@gmail.com>
2015-10-05 09:30:52 +0200

    Added arm to PlayerController

--- src/ga/game/PlayerController.java
+++ src/ga/game/PlayerController.java
@@ -4,7 +4,10 @@ import ga.engine.input.Input;
 import ga.engine.physics.Body;
 import ga.engine.physics.RigidBody;
 import ga.engine.physics.Vector2D;
+import ga.engine.physics.Vector3D;
+import ga.engine.rendering.ImageRenderer;
 import ga.engine.scene.GameComponent;
+import ga.engine.scene.GameObject;
 import javafx.scene.input.KeyCode;
 
 public class PlayerController extends GameComponent {
@@ -13,10 +16,16 @@ public class PlayerController extends GameComponent {
     private final double SPEED = 0.2;
     private final double JUMP_HEIGHT = 6;
     private Vector2D movement;
+    private GameObject arm;
+    private Vector3D armRotation;
     
     @Override
     public void start() {
         body = (RigidBody) getComponent(RigidBody.class);
+        arm = new GameObject()
+                .addComponent(new ImageRenderer("ga/game/RED_Player_Arm.png"));
+        gameobject.addChild(arm);
+        ((ImageRenderer)arm.getRenderable()).setPivot(new Vector2D(8, 3));
     }
 
     @Override
@@ -27,6 +36,8 @@ public class PlayerController extends GameComponent {
         if (Math.abs(body.getVelocity().x) > body.SPEED_LIMIT) {
             body.getVelocity().x = Math.signum(body.getVelocity().x) * body.SPEED_LIMIT;
         }
+        armRotation = new Vector3D(0, 0, (Input.getKey(KeyCode.W) == true ? -5 : 0) + (Input.getKey(KeyCode.S) == true ? 5 : 0));
+        arm.getTransform().rotate(armRotation);
     }
     
     public Vector2D getMovement() {
