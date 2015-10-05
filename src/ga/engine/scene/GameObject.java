5f465291b74da31207f7b52a1061d8975742cf27
Johan Hedlin <busan.katt@gmail.com>
2015-10-05 09:30:36 +0200

    Added rotation pivot to ImageRenderer

--- src/ga/engine/scene/GameObject.java
+++ src/ga/engine/scene/GameObject.java
@@ -198,9 +198,9 @@ public class GameObject {
             node.getTransforms().clear();
             Vector3D rotation = transform.localRotation();
             Vector3D position = transform.localPosition();
-            Rotate rx = new Rotate(rotation.x, 0, 0, 0, Rotate.X_AXIS);
-            Rotate ry = new Rotate(rotation.y, 0, 0, 0, Rotate.Y_AXIS);
-            Rotate rz = new Rotate(rotation.z, 0, 0, 0, Rotate.Z_AXIS);
+            Rotate rx = new Rotate(rotation.x, ((ImageRenderer) renderable).getPivot().x, ((ImageRenderer) renderable).getPivot().y, 0, Rotate.X_AXIS);
+            Rotate ry = new Rotate(rotation.y, ((ImageRenderer) renderable).getPivot().x, ((ImageRenderer) renderable).getPivot().y, 0, Rotate.Y_AXIS);
+            Rotate rz = new Rotate(rotation.z, ((ImageRenderer) renderable).getPivot().x, ((ImageRenderer) renderable).getPivot().y, 0, Rotate.Z_AXIS);
             node.getTransforms().addAll(rx, ry, rz);
             node.setTranslateX((int) position.x);
             node.setTranslateY((int) position.y);
