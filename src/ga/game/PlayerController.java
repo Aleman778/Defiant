fd7ef6812bbe6a4b7e099fadf4843a02e7c3661e
Johan Hedlin <busan.katt@gmail.com>
2015-10-10 14:33:35 +0200

    Separated head sprite from body in PlayerController

--- src/ga/game/PlayerController.java
+++ src/ga/game/PlayerController.java
@@ -15,13 +15,19 @@ public class PlayerController extends GameComponent {
     private RigidBody body;
     private final double SPEED = 0.2;
     private final double JUMP_HEIGHT = 6;
+    private final float HEAD_ROTATION_LIMIT_UPPER = -20;
+    private final float HEAD_ROTATION_LIMIT_LOWER = 45;
     private Vector2D movement;
-    private GameObject arm;
-    private float armRotation;
+    private GameObject head, arm;
+    private float armRotation, headRotation;
     
     @Override
     public void start() {
         body = (RigidBody) getComponent(RigidBody.class);
+        head = new GameObject(0, -21, 0)
+                .addComponent(new ImageRenderer("ga/game/Red_Player_Head.png"));
+        gameobject.addChild(head);
+        ((ImageRenderer)head.getRenderable()).setPivot(new Vector2D(9, 16));
         arm = new GameObject(12, -8, 0)
                 .addComponent(new ImageRenderer("ga/game/Red_arm2ss.png"));
         gameobject.addChild(arm);
@@ -39,15 +45,21 @@ public class PlayerController extends GameComponent {
         
         Vector2D diff = Input.getMousePosition().sub(gameobject.transform.localPosition().toVector2D());
         armRotation = (float) Math.toDegrees(Math.atan(diff.y / diff.x));
+        headRotation = armRotation;
         if (diff.x < 0) {
             armRotation = 360 - armRotation;
             transform.rotation.y = 180;
+            headRotation = Math.max(armRotation, 360 + HEAD_ROTATION_LIMIT_UPPER);
+            headRotation = Math.min(headRotation, 360 + HEAD_ROTATION_LIMIT_LOWER);
         }
         else {
             arm.getTransform().rotation.x = 0;
             transform.rotation.y = 0;
+            headRotation = Math.max(armRotation, HEAD_ROTATION_LIMIT_UPPER);
+            headRotation = Math.min(headRotation, HEAD_ROTATION_LIMIT_LOWER);
         }
         arm.getTransform().rotation.z = armRotation;
+        head.getTransform().rotation.z = headRotation;
     }
     
     public Vector2D getMovement() {
