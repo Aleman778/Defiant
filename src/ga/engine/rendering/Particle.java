8a5e4323ebc5b1c8ac660be18a5f312b4521a9ff
Johan Hedlin <busan.katt@gmail.com>
2015-10-14 09:40:51 +0200

    Added random size to Particle

--- src/ga/engine/rendering/Particle.java
+++ src/ga/engine/rendering/Particle.java
@@ -4,21 +4,22 @@ import ga.engine.physics.Vector2D;
 
 public class Particle {
     private Vector2D position, velocity;
-    private float size, life;
+    private float size, life, lifeTime;
     private ParticleEmitter emitter;
 
     public Particle(ParticleEmitter emitter, Vector2D position, Vector2D velocity, float size, float life) {
         this.position = position;
         this.velocity = velocity;
-        this.size = size;
+        this.size = (int) (0.75 * size * Math.random() + size / 2);
+        this.lifeTime = life;
         this.life = life;
         this.emitter = emitter;
     }
     
     public boolean update() {
         position = position.add(velocity);
-        emitter.graphics.setGlobalAlpha(life / 100);
-        emitter.graphics.fillOval(position.x, position.y, size, size);
+        emitter.graphics.setGlobalAlpha(life / lifeTime);
+        emitter.graphics.fillOval(position.x, position.y, size * life / lifeTime, size * life / lifeTime);
         life--;
         return life > 0;
     }
