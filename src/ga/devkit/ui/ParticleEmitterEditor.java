4d0c27cb93d7cb0ba50620a79a010cf14cb9fb7a
alema <aleman778.maintheria@gmail.com>
2015-10-16 22:47:43 +0200

    Renamed to ParticleEmitterEditor

---
+++ src/ga/devkit/ui/ParticleEmitterEditor.java
@@ -0,0 +1,36 @@
+package ga.devkit.ui;
+
+import ga.engine.rendering.ParticleEmitter;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.Color;
+
+public class ParticleEmitterEditor extends ParticleEmitter {
+
+    public ParticleEmitterEditor(Canvas canvas, float direction, float spread, float size, int mode, float life, Color color) {
+        super(direction, spread, size, mode, life, color);
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void setMode(int mode) {
+        this.mode = mode;
+    }
+
+    public void setLife(float life) {
+        this.life = life;
+    }
+
+    public void setDirection(float direction) {
+        this.direction = direction;
+    }
+
+    public void setSpread(float spread) {
+        this.spread = spread;
+    }
+
+    public void setSize(float size) {
+        this.size = size;
+    }
+}
