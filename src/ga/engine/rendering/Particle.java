dd7c79115cb5cc37528909d5a5c14b2672c5251a
Aleman778 <aleman778.maintheria@gmail.com>
2015-10-18 20:29:02 +0200

    Updated Rendering System! Problems: - Collision Detection - No test level, level loader

--- src/ga/engine/rendering/Particle.java
+++ src/ga/engine/rendering/Particle.java
@@ -1,25 +1,27 @@
 package ga.engine.rendering;
 
 import ga.engine.physics.Vector2D;
+import javafx.scene.canvas.GraphicsContext;
 
 public class Particle {
     private Vector2D position, velocity;
     private float size, life;
-    private ParticleEmitter emitter;
 
-    public Particle(ParticleEmitter emitter, Vector2D position, Vector2D velocity, float size, float life) {
+    public Particle(Vector2D position, Vector2D velocity, float size, float life) {
         this.position = position;
         this.velocity = velocity;
         this.size = size;
         this.life = life;
-        this.emitter = emitter;
     }
     
     public boolean update() {
         position = position.add(velocity);
-        emitter.graphics.setGlobalAlpha(life / 100);
-        emitter.graphics.fillOval(position.x, position.y, size, size);
         life--;
         return life > 0;
+    }
+    
+    public void render(GraphicsContext g) {
+        g.setGlobalAlpha(life / 100);
+        g.fillOval(position.x, position.y, size, size);
     }
 }
--- src/ga/engine/rendering/ParticleEmitter.java
+++ src/ga/engine/rendering/ParticleEmitter.java
@@ -5,44 +5,36 @@ import ga.engine.physics.Vector2D;
 import ga.engine.scene.GameComponent;
 import java.util.HashSet;
 import java.util.Iterator;
-import javafx.scene.Group;
-import javafx.scene.Node;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.effect.BlendMode;
 import javafx.scene.paint.Color;
 
-public class ParticleEmitter extends GameComponent implements Renderable {
+public class ParticleEmitter extends GameComponent {
 
-    protected Canvas canvas;
-    protected GraphicsContext graphics;
-    protected Color color;
+    private final HashSet<Particle> particles;
     public final static int MODE_CONTINUOUS = 0, MODE_SINGLE = 1;
+    protected Color color;
     protected int mode;
     protected float life, direction, spread, size;
-    private HashSet<Particle> particles = new HashSet<>();
-    private Vector2D localPosition;
 
-    public ParticleEmitter(Vector2D position, float direction, float spread, float size, int mode, float life, Color color) {
+    public ParticleEmitter(float direction, float spread, float size, int mode, float life, Color color) {
         this.mode = mode;
         this.life = life;
         this.direction = direction;
         this.spread = spread;
         this.size = size;
-        this.localPosition = position;
         this.color = color;
-        canvas = new Canvas(1920, 1080);
-        graphics = canvas.getGraphicsContext2D();
+        this.particles = new HashSet<>();
     }
 
     @Override
     public void update() {
         super.update();
-        graphics.setFill(color);
-        graphics.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
         if (mode == MODE_CONTINUOUS) {
             addParticles(1);
         }
-        for (Iterator<Particle> it = particles.iterator(); it.hasNext();) {
+        Iterator<Particle> it = particles.iterator();
+        while (it.hasNext()) {
             Particle p = it.next();
             if (!p.update()) {
                 it.remove();
@@ -57,21 +49,24 @@ public class ParticleEmitter extends GameComponent implements Renderable {
     private void addParticles(int amount) {
         for (int i = 0; i < amount; i++) {
             float dir = (float) (direction + (spread * Math.random() - (spread / 2)));
-            particles.add(new Particle(this, gameobject.getTransform().localPosition().toVector2D().add(localPosition), new Vector2D(Math.cos(Math.toRadians(dir)), Math.sin(Math.toRadians(dir))), size, (int) ((life - 100) * Math.random() + 100)));
+//            particles.add(new Particle(gameobject.getTransform().localPosition(),
+//                    new Vector2D(Math.cos(Math.toRadians(dir)), Math.sin(Math.toRadians(dir))),
+//                    size, (int) ((life - 100) * Math.random() + 100))
+//            );
         }
     }
 
     @Override
-    public void render(Group group) {
-        group.getChildren().add(canvas);
+    public void render(GraphicsContext g) {
+        g.getCanvas().setBlendMode(BlendMode.ADD);
+        Iterator<Particle> it = particles.iterator();
+        while (it.hasNext()) {
+            Particle p = it.next();
+            p.render(g);
+        }
+        g.getCanvas().setBlendMode(null);
     }
-
-    @Override
-    public Node getNode() {
-        return canvas;
-    }
-
-    @Override
+    
     public Rectangle computeAABB() {
         return new Rectangle();
     }
