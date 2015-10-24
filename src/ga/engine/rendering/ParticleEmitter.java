43c04c311b66509ad3a368cc6920200086fb5795
Johan Hedlin <busan.katt@gmail.com>
2015-10-24 16:24:22 +0200

    Fixed particle emitters not working

--- src/ga/engine/rendering/ParticleEmitter.java
+++ src/ga/engine/rendering/ParticleEmitter.java
@@ -2,10 +2,12 @@ package ga.engine.rendering;
 
 import ga.engine.physics.Vector2D;
 import ga.engine.scene.GameComponent;
+import ga.engine.scene.GameObject;
 import java.util.HashSet;
 import java.util.Iterator;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.transform.Affine;
 
 public class ParticleEmitter extends GameComponent {
 
@@ -14,6 +16,8 @@ public class ParticleEmitter extends GameComponent {
     protected Color color;
     protected int mode;
     protected float life, direction, spread, size;
+    public Vector2D offset = new Vector2D();
+    public GameObject object;
 
     public ParticleEmitter(float direction, float spread, float size, int mode, float life, Color color) {
         this.mode = mode;
@@ -23,6 +27,7 @@ public class ParticleEmitter extends GameComponent {
         this.size = size;
         this.color = color;
         this.particles = new HashSet<>();
+        object = new GameObject();
     }
 
     @Override
@@ -47,12 +52,14 @@ public class ParticleEmitter extends GameComponent {
     private void addParticles(int amount) {
         for (int i = 0; i < amount; i++) {
             float dir = (float) (direction + (spread * Math.random() - (spread / 2)));
-            particles.add(new Particle(gameobject.getTransform().localPosition(), new Vector2D(Math.cos(Math.toRadians(dir)), Math.sin(Math.toRadians(dir))), size, (int) ((life - 100) * Math.random() + 100)));
+            particles.add(new Particle(transform.position.add(object.transform.position), new Vector2D(Math.cos(Math.toRadians(dir)), Math.sin(Math.toRadians(dir))), size, (int) ((life - 100) * Math.random() + 100)));
         }
     }
 
     @Override
     public void render(GraphicsContext g) {
+        g.translate(-transform.position.x, -transform.position.y);
+        g.setTransform(new Affine());
         Iterator<Particle> it = particles.iterator();
         while (it.hasNext()) {
             Particle p = it.next();
