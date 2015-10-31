86f7bedb63037bee030b30ff034ff0c776f6c100
alema <aleman778.maintheria@gmail.com>
2015-10-31 14:32:01 +0100

    Added depth to the objecet Transformation

--- src/ga/engine/scene/Transform2D.java
+++ src/ga/engine/scene/Transform2D.java
@@ -9,25 +9,27 @@ public class Transform2D {
     public Vector2D pivot;
     public Vector2D scale;
     public double rotation;
+    public int depth;
 
-    public Transform2D(GameObject object, Vector2D position, Vector2D pivot, double rotation, Vector2D scale) {
+    public Transform2D(GameObject object, Vector2D position, Vector2D pivot, double rotation, Vector2D scale, int depth) {
         this.gameobject = object;
         this.position = position;
         this.pivot = pivot;
         this.rotation = rotation;
         this.scale = scale;
+        this.depth = depth;
     }
     
     public Transform2D(GameObject object, double x, double y) {
-        this(object, new Vector2D(x, y), new Vector2D(), 0f, new Vector2D(1, 1));
+        this(object, new Vector2D(x, y), new Vector2D(), 0f, new Vector2D(1, 1), 0);
     }
     
     public Transform2D(GameObject object, Transform2D transform) {
-        this(object, transform.position, transform.pivot, transform.rotation, transform.scale);
+        this(object, transform.position, transform.pivot, transform.rotation, transform.scale, transform.depth);
     }
     
     public Transform2D(GameObject object) {
-        this(object, new Vector2D(), new Vector2D(), 0f, new Vector2D(1, 1));
+        this(object, new Vector2D(), new Vector2D(), 0f, new Vector2D(1, 1), 0);
     }
     
     public void translate(Vector2D vector2) {
@@ -62,5 +64,13 @@ public class Transform2D {
         if (parent != null)
             return parent.getTransform().localRotation() + rotation;
         return rotation;
+    }
+
+    public double getDepth() {
+        return depth;
+    }
+
+    public void setDepth(int depth) {
+        this.depth = depth;
     }
 }
