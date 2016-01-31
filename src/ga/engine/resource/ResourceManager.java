2cd435b02989eab222f11dcd05bcc0dc2b4fcbbd
alema <aleman778.maintheria@gmail.com>
2016-01-31 23:12:25 +0100

    Added support for blueprints

--- src/ga/engine/resource/ResourceManager.java
+++ src/ga/engine/resource/ResourceManager.java
@@ -1,5 +1,9 @@
 package ga.engine.resource;
 
+import ga.engine.blueprint.Blueprint;
+import ga.engine.blueprint.BlueprintParser;
+import ga.engine.scene.GameComponent;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import javafx.scene.image.Image;
@@ -7,10 +11,12 @@ import javafx.scene.image.Image;
 public final class ResourceManager {
     
     public static final Image DEFAULT_IMAGE = new Image("textures/nonrenderable.png");
+    public static final Blueprint DEFAULT_BLUEPRINT = new Blueprint(new ArrayList<>(), new HashMap<>());
     
-    private static Map<String, Image> images = new HashMap<>();
+    private static final Map<String, Image> images = new HashMap<>();
+    private static final Map<String, Blueprint> blueprints = new HashMap<>();
     
-    public static Image get(String filepath) {
+    public static Image getImage(String filepath) {
         if (images.containsKey(filepath)) {
             return images.get(filepath);
         }
@@ -23,8 +29,22 @@ public final class ResourceManager {
         images.put(filepath, image);
         return image;
     }
+
+    public static Blueprint getBlueprint(String filepath) {
+        if (blueprints.containsKey(filepath)) {
+            return blueprints.get(filepath);
+        }
+        Blueprint blueprint;
+        try {
+            blueprint = BlueprintParser.execute(filepath);
+        } catch (Exception e) {
+            blueprint = DEFAULT_BLUEPRINT;
+        }
+        blueprints.put(filepath, blueprint);
+        return blueprint;
+    }
     
-    public static void remove(String filepath) {
+    public static void unloadImage(String filepath) {
         if (!images.containsKey(filepath))
             return;
         
