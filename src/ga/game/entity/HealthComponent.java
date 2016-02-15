2a393d1b702ac8d321478392c50e863e041fb17a
Johan Hedlin <busan.katt@gmail.com>
2016-02-15 10:31:09 +0100

    Added healthbar to entities

--- src/ga/game/entity/HealthComponent.java
+++ src/ga/game/entity/HealthComponent.java
@@ -4,41 +4,46 @@ import ga.engine.scene.GameComponent;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.transform.Affine;
 
 public class HealthComponent extends GameComponent {
-    
+
     private static final List<String> ATTRIBUTES = new ArrayList<>();
-    
+
     static {
         ATTRIBUTES.add("Health");
     }
-    
-    private double health;
-    
+
+    private double health, startHealth;
+
     public HealthComponent() {
         this.health = 1;
     }
-    
+
     public void setHealth(double health) {
         this.health = health;
+        startHealth = health;
     }
-    
+
     public double getHealth() {
         return health;
     }
-    
+
     public void heal(double amount) {
+        amount = Math.min(amount, startHealth - amount);
         this.health += amount;
     }
-    
+
     public void damage(double amount) {
+        amount = Math.min(amount, health);
         this.health -= amount;
         AI ai = (AI) getComponent(AI.class);
         if (ai != null) {
             ai.takingDamage = amount;
         }
     }
-    
+
     public boolean isAlive() {
         return health > 0;
     }
@@ -51,6 +56,16 @@ public class HealthComponent extends GameComponent {
     }
 
     @Override
+    public void render(GraphicsContext g) {
+        if (!gameobject.getTag().equals("player")) {
+            Affine a = g.getTransform();
+            Affine b = new Affine(1, a.getMxy(), transform.position.x, a.getMyx(), a.getMyy(), transform.position.y - 15);
+            g.setTransform(b);
+            g.fillRect(0, 0, transform.gameobject.getAABB().width * (health / startHealth), 10);
+        }
+    }
+
+    @Override
     public List<String> getAttributes() {
         return ATTRIBUTES;
     }
@@ -58,8 +73,9 @@ public class HealthComponent extends GameComponent {
     @Override
     public void setAttributes(Map<String, String> attributes) {
         health = Double.parseDouble(attributes.get("Health"));
+        startHealth = health;
     }
-    
+
     @Override
     public GameComponent instantiate() {
         return new HealthComponent();
